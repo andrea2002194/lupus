e13b5c16bcbe167cc42b245cd438cebfbe079cc0
andrea2002194 <nc76rvs9yp@privaterelay.appleid.com>
2026-01-05 01:44:17 +0100

    Aggiornamento PWA vlupus-cache- 05/01/2026  1:44:17.10

--- aggiorna-pwa.bat
+++ aggiorna-pwa.bat
@@ -8,25 +8,26 @@ set PWA_DIR=C:\Users\vavas\OneDrive\Desktop\pwa
 cd /d "%PWA_DIR%"
 
 REM -------------------------------
-REM Incrementa la versione della cache in sw.js
-REM Cerca 'CACHE_NAME = lupus-cache-vX' e incrementa X
+REM Incrementa la versione della cache in sw.js usando PowerShell
 REM -------------------------------
-setlocal enabledelayedexpansion
-set FILE=sw.js
-for /f "tokens=1,2 delims=v" %%a in ('findstr /c:"CACHE_NAME" %FILE%') do (
-    set VERSION=%%b
-)
-set /a VERSION_NEW=VERSION+1
-echo Nuova versione della cache: v!VERSION_NEW!
+powershell -Command ^
+$swFile = '%PWA_DIR%\sw.js'; ^
+$content = Get-Content $swFile; ^
+$content = $content -replace 'CACHE_NAME\s*=\s*''lupus-cache-v(\d+)''', { 
+    $num = [int]$args[0]; 'CACHE_NAME = ''lupus-cache-v' + ($num+1) + '''' 
+}; ^
+Set-Content $swFile $content
 
-REM Sostituisce la versione vecchia con quella nuova
-powershell -Command "(Get-Content '%FILE%') -replace 'lupus-cache-v[0-9]+','lupus-cache-v!VERSION_NEW!' | Set-Content '%FILE%'"
+REM Legge la nuova versione della cache
+for /f "tokens=2 delims=v'" %%a in ('findstr "CACHE_NAME" sw.js') do set VERSION_NEW=%%a
+
+echo Nuova versione della cache: v%VERSION_NEW%
 
 REM -------------------------------
 REM Git add, commit e push
 REM -------------------------------
 git add .
-git commit -m "Aggiornamento PWA v!VERSION_NEW! %date% %time%"
+git commit -m "Aggiornamento PWA v%VERSION_NEW% %date% %time%"
 git pull origin main --rebase
 git push origin main
 
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'lupus-cache-v1';
+const CACHE_NAME = 'lupus-cache-v7';
 
 // Lista di tutti i file da precacheare
 const FILES_TO_CACHE = [
